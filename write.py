3bd383331d56ddc65bb2d3ecccd26c6645bce891
ido sarue <dodojoker67@gmail.com>
2021-11-03 20:36:54 +0200

    started learning about s&op

---
+++ write.py
@@ -0,0 +1,14 @@
+info = '''
+    You can work on your SQL and read about:
+
+    S&OP.
+    Demand Forecasting/Planning.
+    Oracle Demand Management Cloud.
+    Demand Sensing.
+    Trade Promotions.
+'''
+
+with open('what_to_learn.txt', 'w') as file:
+    file.write(info)
+
+  
